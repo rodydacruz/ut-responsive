d3b62def59d54a5700970876ac8eda1348868896
rodydacruz <rodylag@outlook.com>
2021-11-05 19:21:15 +0000

    adding bootstrap

---
+++ uniteltmais/comps/navbar.js
@@ -0,0 +1,18 @@
+
+const NavBar = () => {
+
+    return(
+        <div>
+            <a>Oferta</a>
+            <a>Unitel PRO</a>
+            <a>Unitel Tmais</a>
+            <a>Cliente +</a>
+            <a>Casa +</a>
+            <a>Univel Web</a>
+            <a>Casa +TV</a>
+        </div>
+    );
+    
+}
+export default NavBar;
+

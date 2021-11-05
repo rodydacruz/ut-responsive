b6f258d2777903fc5f65369dc90bf851b9dfc4f5
rodydacruz <rodylag@outlook.com>
2021-11-05 20:15:08 +0000

    Ok

--- uniteltmais/comps/Layout.js
+++ uniteltmais/comps/Layout.js
@@ -1,11 +1,11 @@
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Header from "./Header";
 
 
 const Layout = ({children}) => {
     return(
         <div>
-            <Navbar />
+            <Header />
             {children}
             <Footer />
         </div>
--- uniteltmais/comps/Navbar.js
+++ uniteltmais/comps/Navbar.js
@@ -1,8 +1,11 @@
-
+import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
 const Navbar = () => {
 
     return(
         <div>
+            <Button className={styles.nb}>Oferta</Button>
             <a>Oferta</a>
             <a>Unitel PRO</a>
             <a>Unitel Tmais</a>

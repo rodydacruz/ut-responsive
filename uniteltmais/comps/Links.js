edd93d8210f250a498b465fbefb717c3be26dcdb
rodydacruz <rodylag@outlook.com>
2021-11-11 15:38:16 +0000

    ok

--- uniteltmais/comps/Links.js
+++ uniteltmais/comps/Links.js
@@ -6,10 +6,10 @@ import { Facebook,Instagram,Youtube,Twitter,Pinterest } from 'react-bootstrap-ic
 export default function Links(){
     return(
         <div>
-        <Link href="https://www.facebook.com/rdc.rodydacruz"><a target="_blank"><Facebook /></a></Link>
-        <Link href="https://twitter.com/RODYDACRUZ"><a target="_blank"><Twitter /></a></Link>
-        <Link href="https://www.youtube.com/channel/UCH1oMlEtrhWpq42f4O9rvMg"><a target="_blank"><Youtube /></a></Link>
-        <Link href="https://www.instagram.com/rodydacruz/s"><a target="_blank"><Instagram /></a></Link>
+        <Link href="https://www.facebook.com/rdc.rodydacruz"><a target="_blank"><Facebook size="5em"/></a></Link>
+        <Link href="https://twitter.com/RODYDACRUZ"><a target="_blank"><Twitter size="5em"/></a></Link>
+        <Link href="https://www.youtube.com/channel/UCH1oMlEtrhWpq42f4O9rvMg"><a target="_blank"><Youtube size="5em"/></a></Link>
+        <Link href="https://www.instagram.com/rodydacruz/s"><a target="_blank"><Instagram size="5em"/></a></Link>
         <Link href="/"><a></a></Link>
     </div>
     )

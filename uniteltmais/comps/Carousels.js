ed444a56ddfa4edfab9a52d575933596edd29850
rodydacruz <rodylag@outlook.com>
2021-11-11 18:38:15 +0000

    adding swipe to carousel

--- uniteltmais/comps/Carousels.js
+++ uniteltmais/comps/Carousels.js
@@ -3,7 +3,7 @@ import Carousel from 'react-bootstrap/Carousel'
 export default function Carousels() {
     return (
         
-        <Carousel variant="dark">
+        <Carousel variant="dark" touch>
         <Carousel.Item>
           <img
             className="d-block w-100"
--- uniteltmais/comps/Navbar.js
+++ uniteltmais/comps/Navbar.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Button from 'react-bootstrap/Button'
+import BImage from 'react-bootstrap/Image'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Navbar = () => {
@@ -7,14 +8,19 @@ const Navbar = () => {
     return(
         
         <div>
-            <Button className={styles.nb}>Oferta</Button>
-            <a>Oferta</a>
-            <a>Unitel PRO</a>
-            <a>Unitel Tmais</a>
-            <a>Cliente +</a>
-            <a>Casa +</a>
-            <a>Univel Web</a>
-            <a>Casa +TV</a>
+            <Button variant="flat" className={styles.nb}>+ OFERTA</Button>
+            <Button variant="flat" className={styles.nb}>UNITEL PRO +</Button>
+            <Button variant="flat" className={styles.nb}>Unitel Tmais</Button>
+            <Button variant="flat" className={styles.nb}><BImage src="Logo-Cliente copy.png" /></Button>
+            <Button variant="flat" className={styles.nb}><BImage src="logo-casa+.png" /></Button>
+            <Button variant="flat" className={styles.nb}><BImage src="Logo-Cliente copy.png" /></Button>
+            <Button variant="flat" className={styles.nb}><BImage src="Botton_Menu4.png" /></Button>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
             </div>
     );
     
